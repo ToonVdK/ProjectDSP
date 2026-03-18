2365ed5a7e56f1b1ca76285a0c4f2751281eaf7f
Robeen <robeendoci1908@gmail.com>
2026-03-18 14:20:22 +0100

    app design started

---
+++ application.py
@@ -0,0 +1,39 @@
+import PyQt6 as qt
+import PyQt6.QtWidgets as widget
+
+# Only needed for access to command line arguments
+import sys
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+
+class MainWindow(widget.QMainWindow):
+    def __init__(self):
+        widget.QMainWindow.__init__(self)
+
+        self.setWindowTitle("HeartBP")
+        # self.showFullScreen()
+        self.setFixedSize(800, 600)
+        button = widget.QPushButton("Start")
+        button.setCheckable(True)
+        button.clicked.connect(self.buttonPressed())
+
+        #set central object of window
+        self.setCentralWidget(button)
+
+    def buttonPressed(self):
+        print("Button pressed")
+
+app = widget.QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
+
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
