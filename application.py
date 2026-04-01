91c34f0aed737810e7e958660e2707aaf0d4ab1c
Robeen <robeendoci1908@gmail.com>
2026-04-01 14:34:09 +0200

    app splash screen

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
+import time
+
 import PyQt6 as qt
 import PyQt6.QtWidgets as widget
 import PyQt6.QtGui as gui
 import sys
 
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import QFileDialog
 
 """
@@ -110,6 +113,20 @@ app = widget.QApplication(sys.argv)
 app.setApplicationName("HeartBP")
 app.setApplicationVersion("1.0")
 app.setWindowIcon(gui.QIcon("img/icon.png"))
+
+splash_pix = gui.QPixmap("img/heartBP.png").scaled(800, 800)
+splash = widget.QSplashScreen(splash_pix, QtCore.Qt.WindowType.WindowStaysOnTopHint)
+# add fade to splashscreen
+opaqueness = 0.0
+step = 0.02
+splash.setWindowOpacity(opaqueness)
+splash.show()
+while opaqueness < 1:
+    splash.setWindowOpacity(opaqueness)
+    time.sleep(step) # Gradually appears
+    opaqueness+=step
+time.sleep(1) # hold image on screen for a while
+splash.close() # close the splash screen
 
 # Create a Qt widget, which will be our window.
 window = MainWindow()
